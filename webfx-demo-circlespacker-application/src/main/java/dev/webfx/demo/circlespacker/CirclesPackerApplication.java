839e79b557fbc8fd769f573fe059fc9341e766c4
Bruno Salmon <dev.salmonb@gmail.com>
2022-08-10 11:36:59 +0100

    Added vertical split pane

--- webfx-demo-circlespacker-application/src/main/java/dev/webfx/demo/circlespacker/CirclesPackerApplication.java
+++ webfx-demo-circlespacker-application/src/main/java/dev/webfx/demo/circlespacker/CirclesPackerApplication.java
@@ -2,12 +2,14 @@ package dev.webfx.demo.circlespacker;
 
 import dev.webfx.lib.circlespacker.CirclesPackerPane;
 import javafx.application.Application;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+
+import java.util.stream.IntStream;
 
 /**
  * @author Bruno Salmon
@@ -16,23 +18,18 @@ public class CirclesPackerApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setScene(new Scene(new SplitPane(createMostlyFluidPane(0), createMostlyFluidPane(100)), 850, 450));
+        SplitPane verticalSplitPane;
+        primaryStage.setScene(new Scene(verticalSplitPane = new SplitPane(
+                new SplitPane(new CirclesPackerPane(createCircles(5, Color.CADETBLUE)), new CirclesPackerPane(createCircles(8, Color.BLUEVIOLET))),
+                new CirclesPackerPane(createCircles(13, Color.SANDYBROWN))
+        ), 850, 450));
+        verticalSplitPane.setOrientation(Orientation.VERTICAL);
         primaryStage.setTitle("Circles Packer");
         primaryStage.show();
     }
 
-    private static Pane createMostlyFluidPane(double hueShift) {
-        Circle a1 = createArea(Color.rgb(0, 52, 118), hueShift);
-        Circle a2 = createArea(Color.rgb(0, 98, 210), hueShift);
-        Circle a3 = createArea(Color.rgb(180, 210, 247), hueShift);
-        Circle a4 = createArea(Color.rgb(213, 223, 239), hueShift);
-        Circle a5 = createArea(Color.rgb(223, 225, 229), hueShift);
-        return new CirclesPackerPane(a1, a2, a3, a4, a5);
+    private static Circle[] createCircles(int count, Color color) {
+        return IntStream.range(0, count).mapToObj(i -> new Circle(0, color)).toArray(Circle[]::new);
     }
 
-    private static Circle createArea(Color color, double hueShift) {
-        Circle area = new Circle();
-        area.setFill(color.deriveColor(hueShift, 1, 1, 1));
-        return area;
-    }
 }
