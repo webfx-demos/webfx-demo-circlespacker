a9162719ec2608f393972b1bee097170f2868031
Bruno Salmon <dev.salmonb@gmail.com>
2022-08-12 16:03:16 +0100

    Renamed demo to webfx-demo-ledpacking

---
+++ webfx-demo-ledpacking-application/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+// File managed by WebFX (DO NOT EDIT MANUALLY)
+
+module webfx.demo.ledpacking.application {
+
+    // Direct dependencies modules
+    requires java.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires webfx.extras.led;
+    requires webfx.lib.circlepacking;
+
+    // Exported packages
+    exports dev.webfx.demo.ledpacking;
+
+    // Provided services
+    provides javafx.application.Application with dev.webfx.demo.ledpacking.LedPackingApplication;
+
+}
