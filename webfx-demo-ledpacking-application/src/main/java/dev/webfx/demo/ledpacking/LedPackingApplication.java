475396afd1a4b1be58817a8f980ef3e3b061886c
Bruno Salmon <dev.salmonb@gmail.com>
2022-08-14 19:46:27 +0100

    Refactored methods in main application class with clearer names

--- webfx-demo-ledpacking-application/src/main/java/dev/webfx/demo/ledpacking/LedPackingApplication.java
+++ webfx-demo-ledpacking-application/src/main/java/dev/webfx/demo/ledpacking/LedPackingApplication.java
@@ -21,27 +21,28 @@ import java.util.stream.IntStream;
  */
 public class LedPackingApplication extends Application {
 
+    // Used to bing all led highlighted properties together
     private final BooleanProperty highlightedProperty = new SimpleBooleanProperty();
 
     @Override
     public void start(Stage primaryStage) {
         SplitPane verticalSplitPane;
         primaryStage.setScene(new Scene(verticalSplitPane = new SplitPane(
-                new SplitPane(createCirclesPackerPane(5, Color.CADETBLUE), createCirclesPackerPane(8, Color.BLUEVIOLET)),
-                createCirclesPackerPane(13, Color.SANDYBROWN)
+                new SplitPane(createLedPackingPane(5, Color.CADETBLUE), createLedPackingPane(8, Color.BLUEVIOLET)),
+                createLedPackingPane(13, Color.SANDYBROWN)
         ), 850, 450));
         verticalSplitPane.setOrientation(Orientation.VERTICAL);
         primaryStage.setTitle("Led Packing");
         primaryStage.show();
     }
 
-    private CirclePackingPane createCirclesPackerPane(int count, Color color) {
-        CirclePackingPane circlePackingPane = new CirclePackingPane(createCircles(count, color));
-        circlePackingPane.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
-        return circlePackingPane;
+    private CirclePackingPane createLedPackingPane(int count, Color ledColor) {
+        CirclePackingPane ledPackingPane = new CirclePackingPane(createLeds(count, ledColor));
+        ledPackingPane.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+        return ledPackingPane;
     }
 
-    private Node[] createCircles(int count, Color color) {
+    private Node[] createLeds(int count, Color color) {
         return IntStream.range(0, count).mapToObj(i -> createLed(color.brighter().brighter())).toArray(Node[]::new);
     }
 
